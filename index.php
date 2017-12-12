3946044361a78057a3c179b07261cf8d1a0ac100
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 13:57:00 +0100

    ajout exo4

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
-<?php
-$numberOne = 1;
-$result = $numberOne / 2;
-while (($numberOne + $result) <=10) {
-  $result = $numberOne / 2;
-  $numberOne = $numberOne + $result;
-  echo $numberOne.'<br/>';
-}
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 4 partie 3 php</title>
+  </head>
+  <body>
+    <?php
+    function calculate() {
+      $number1 = 32;
+      $number2 = 42;
+      if ($number1 > $number2) {
+        return 'le premier nombre est plus grand';
+      }
+      elseif ($number1 < $number2) {
+        return 'le premier nombre est plus petit';
+      }
+      elseif ($number1 == $number2) {
+        return 'les deux nombres sont identiques';
+      }
+    }
+    echo calculate();
+    ?>
+  </body>
+</html>
